723c22b68e18eb8b3829751afb87c54963d4e371
YuriyNik156 <yuriy.nik156@gmail.com>
2025-07-01 15:20:10 +0300

    create new django project

---
+++ auth_project/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # заменишь на нужный URL
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'main/register.html', {'form': form})
+
+def home(request):
+    return render(request, 'main/home.html')
